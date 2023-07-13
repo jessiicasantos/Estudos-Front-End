4c17e1efc4ec32ac8e7287de392f9bfcb9ed3682
email <jessiicansantos@gmail.com>
2023-07-13 19:34:22 -0300

    Projeto em React adicionado: villa-h_react

---
+++ React/villa-h_react/src/components/Header/Nav/NavLogo/NavLogo.jsx
@@ -0,0 +1,11 @@
+import { ReactComponent as LogoType } from '../img/logotype-villa-h.svg';
+
+const NavLogo = () => (
+  <div className="nav-logo">
+    <a href="#momentos">
+      <LogoType />
+    </a>
+  </div>
+);
+
+export default NavLogo;
