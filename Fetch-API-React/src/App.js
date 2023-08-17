b872ffc6bfd219e4708446cf301ee1576fc712d8
email <jessiicansantos@gmail.com>
2023-08-17 18:31:30 -0300

    Material UI added

--- Fetch-API-React/src/App.js
+++ Fetch-API-React/src/App.js
@@ -1,4 +1,9 @@
 import { useState, useEffect } from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
 import './App.css';
 
 // draft
@@ -27,6 +32,7 @@ const App = () => {
   return (
     <div>
       {JSON.stringify(data)}
+      <Button variant="contained">Hello world</Button>
     </div>
   )
 }
